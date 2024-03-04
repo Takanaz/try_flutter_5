b0753318e2c3b9e425cffae88afc89b8372d150e
takanaz <tanazawa@cosmgate.io>
2024-03-04 12:26:58 +0900

    create account page and fix chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
 import 'package:try_flutter_5/firebase_options.dart';
 import 'package:try_flutter_5/post.dart';
+import 'package:try_flutter_5/my_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,19 +18,30 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
+final user = FirebaseAuth.instance.currentUser;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SignInPage(),
-    );
+    if (user == null) {
+      return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SignInPage(),
+      );
+    } else {
+      return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const ChatPage(),
+      );
+    }
   }
 }
 
@@ -99,58 +114,185 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  Future<void> sendPost(String text) async {
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final posterId = user.uid;
+    final posterName = user.displayName!;
+    final posterImageUrl = user.photoURL!;
+
+    final newDocumentReference = postsReference.doc();
+
+    final newPost = Post(
+      text: text,
+      createdAt: Timestamp.now(),
+      posterName: posterName,
+      posterImageUrl: posterImageUrl,
+      posterId: posterId,
+      reference: newDocumentReference,
+    );
+
+    newDocumentReference.set(newPost);
+  }
+
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot<Post>>(
-                stream: postsReference.orderBy('createdAt').snapshots(),
-                builder: (context, snapshot) {
-                  final docs = snapshot.data?.docs ?? [];
-                  return ListView.builder(
-                    itemCount: docs.length,
-                    itemBuilder: (context, index) {
-                      final post = docs[index].data();
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(post.posterImageUrl),
-                        ),
-                        title: Text(post.posterName),
-                        subtitle: Text(post.text),
-                      );
-                    },
-                  );
+    return GestureDetector(
+      onTap: () {
+        primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('チャット'), actions: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  return const MyPage();
                 }),
-          ),
-          TextFormField(
-            onFieldSubmitted: (text) {
-              final user = FirebaseAuth.instance.currentUser!;
-
-              final posterId = user.uid;
-              final posterName = user.displayName!;
-              final posterImageUrl = user.photoURL!;
-
-              final newDocumentReference = postsReference.doc();
-
-              final newPost = Post(
-                text: text,
-                createdAt: Timestamp.now(),
-                posterName: posterName,
-                posterImageUrl: posterImageUrl,
-                posterId: posterId,
-                reference: newDocumentReference,
               );
-
-              newDocumentReference.set(newPost);
             },
-          ),
-        ],
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                FirebaseAuth.instance.currentUser!.photoURL!,
+              ),
+            ),
+          )
+        ]),
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<QuerySnapshot<Post>>(
+                  stream: postsReference.orderBy('createdAt').snapshots(),
+                  builder: (context, snapshot) {
+                    final docs = snapshot.data?.docs ?? [];
+                    return ListView.builder(
+                      itemCount: docs.length,
+                      itemBuilder: (context, index) {
+                        final post = docs[index].data();
+                        return PostWidget(post: post);
+                      },
+                    );
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: controller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: Colors.amber, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: Colors.amber, width: 2),
+                  ),
+                  fillColor: Colors.amber[50],
+                  filled: true,
+                ),
+                onFieldSubmitted: (text) {
+                  sendPost(text);
+                  controller.clear();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({
+    super.key,
+    required this.post,
+  });
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(post.posterImageUrl),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        post.posterName,
+                        style: const TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                          DateFormat('MM/dd HH:mm')
+                              .format(post.createdAt.toDate()),
+                          style: const TextStyle(fontSize: 10)),
+                    ],
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: user!.uid == post.posterId
+                              ? Colors.amber[100]
+                              : Colors.blue[100]),
+                      child: Text(post.text)),
+                  if (user!.uid == post.posterId)
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text('編集'),
+                                    content: TextFormField(
+                                      initialValue: post.text,
+                                      autofocus: true,
+                                      onFieldSubmitted: (newText) {
+                                        post.reference
+                                            .update({'text': newText});
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  );
+                                });
+                          },
+                          icon: const Icon(Icons.edit),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            post.reference.delete();
+                          },
+                          icon: const Icon(Icons.delete),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/my_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:try_flutter_5/main.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    return Scaffold(
+        appBar: AppBar(title: const Text('マイページ')),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  user.photoURL!,
+                ),
+                radius: 40.0,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                user.displayName!,
+                style: const TextStyle(
+                    fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('ユーザーID：${user.uid}')),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('登録日：${user.metadata.creationTime!}')),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () async {
+                  await GoogleSignIn().signOut();
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) {
+                      return const SignInPage();
+                    }),
+                    (route) => false,
+                  );
+                },
+                child: const Text('サインアウト'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
